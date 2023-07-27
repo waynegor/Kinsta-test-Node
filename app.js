b0f496b6d95439b6a97f1d57a7fd1f04b720b867
Wayne Gordon <105268365+waynegor@users.noreply.github.com>
2023-07-27 15:08:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const http = require('http');
 
-const port = 8080;
+const port = process.env.PORT;;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
